14b06f651ed22b0dc22077426c8f6e78b1af94b9
samckp <sam.mca.liet@gmail.com>
2021-10-09 06:53:47 +0530

    star pattern

--- src/main/java/edu/java8/poc/StarPattern.java
+++ src/main/java/edu/java8/poc/StarPattern.java
@@ -1,2 +1,23 @@
-package edu.java8.poc;public class StarPattern {
-}
+package edu.java8.poc;
+
+// Netcracker Interview on 5th-October-2021
+
+public class StarPattern {
+    public static void main(String[] args) {
+
+            int i, j, n=5;
+
+            for(i=0; i<n; i++) //an outer loop for number of rows(n)
+            {
+                for(j=2*(n-i); j>=0; j--) // an inner loop for spaces
+                {
+                    System.out.print(" "); // to print space
+                }
+                for(j=0; j<=i; j++) // an inner loop for columns
+                {
+                    System.out.print("* "); // to print star
+                }
+                 System.out.println(); // end-line after every row
+            }
+        }
+    }
