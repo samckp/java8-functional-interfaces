d2464737700a297cf78dfec30806d3747a38ab98
samckp <sam.mca.liet@gmail.com>
2021-10-30 08:27:17 +0530

    test prog

--- src/main/java/edu/java8/poc/Test.java
+++ src/main/java/edu/java8/poc/Test.java
@@ -1,2 +1,23 @@
-package edu.java8.poc;public class Test {
+package edu.java8.poc;
+
+class Test1{
+    void msg()throws Exception {            //Narrowing is allowed in Overriding concept
+        System.out.println("parent method");
+    }
+}
+
+public class Test extends Test1 {
+
+    void msg() throws ArithmeticException {
+
+        System.out.println("child method");
+    }
+
+    public static void main(String args[]) {
+        Test1 p = new Test();
+        try {
+            p.msg();
+        } catch (Exception e) {
+        }
+    }
 }
