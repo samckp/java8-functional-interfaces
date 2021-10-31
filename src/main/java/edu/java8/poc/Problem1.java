f9719e9a0d6e977327df1406b22e36599be16b05
samckp <sam.mca.liet@gmail.com>
2021-10-31 09:37:36 +0530

    refactored

--- src/main/java/edu/java8/poc/Problem1.java
+++ src/main/java/edu/java8/poc/Problem1.java
@@ -1,2 +1,28 @@
-package edu.java8.poc;public class Problem1 {
+package edu.java8.poc;
+
+import java.util.Arrays;
+
+public class Problem1 {
+    public static void main(String[] args) {
+
+        int []arr ={1,2, 3, 4, 4, 5, 6 ,7, 8, 8, 9,9};
+
+        Arrays.sort(arr);
+
+        int tmp, j=0;
+
+        for(int i=0; i<arr.length - 1;i++){
+
+            if((arr[i] != arr[i+1] ) ){
+
+                arr[j] = arr[i];
+                j++;
+            }
+        }
+
+        arr[j] = arr[arr.length -1];
+
+        for(int i=0;i<=j;i++)
+            System.out.println(arr[i]);
+    }
 }
